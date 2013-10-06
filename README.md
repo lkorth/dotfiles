97147c77131b105c592c984096fce0caf418a748
lkorth <luke@lukekorth.com>
2013-10-06 22:35:17 +0000

    Add install script

---
+++ README.md
@@ -0,0 +1,5 @@
+# dotfiles
+
+## Installing
+
+`curl -sS https://raw.github.com/lkorth/dotfiles/master/install.sh | sh`
---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+wget -O ~/.tmux.conf https://raw.github.com/lkorth/dotfiles/master/tmux.conf
+wget -O ~/.vimrc https://raw.github.com/lkorth/dotfiles/master/vimrc
+wget -O ~/.gitconfig https://raw.github.com/lkorth/dotfiles/master/gitconfig
