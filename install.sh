0c6ccbbb56892cbe107de93389a6b0573e6a4de2
lkorth <luke@lukekorth.com>
2013-12-20 18:20:06 -0500

    Make sure wget overwrites existing dotfiles

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-wget -O ~/.tmux.conf https://raw.github.com/lkorth/dotfiles/master/tmux.conf
-wget -O ~/.vimrc https://raw.github.com/lkorth/dotfiles/master/vimrc
-wget -O ~/.gitconfig https://raw.github.com/lkorth/dotfiles/master/gitconfig
+wget -N -O ~/.tmux.conf https://raw.github.com/lkorth/dotfiles/master/tmux.conf
+wget -N -O ~/.vimrc https://raw.github.com/lkorth/dotfiles/master/vimrc
+wget -N -O ~/.gitconfig https://raw.github.com/lkorth/dotfiles/master/gitconfig
 
 mkdir -p ~/.vim/autoload
 mkdir -p ~/.vim/bundle
-wget -O ~/.vim/autoload/pathogen.vim https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
+wget -N -O ~/.vim/autoload/pathogen.vim https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
 rm -rf ~/.vim/bundle/nerdtree
 git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
