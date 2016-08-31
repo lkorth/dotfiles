f91da036783de98d5a6a91cee5932424be3af7ca
lkorth <luke@lukekorth.com>
2016-08-31 18:23:48 -0400

    Add script to serve the current directory over http
    Thanks @evanhahn!
    
    https://github.com/EvanHahn/dotfiles/blob/e350c50b5e4fa2a75f263d9ad9f1209d74f4d880/home/bin/bin/serveit

--- install.sh
+++ install.sh
@@ -7,6 +7,11 @@ ln -s $(pwd)/zshrc ~/.zshrc
 ln -s $(pwd)/vimrc ~/.vimrc
 ln -s $(pwd)/gitconfig ~/.gitconfig
 
+# create .bin directory
+rm -rf ~/.bin
+mkdir ~/.bin
+cp -r bin/* ~/.bin
+
 # setup vim
 rm -rf ~/.vim
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
